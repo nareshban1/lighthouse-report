50c8cef1f11e959739b8beb3a9cb74878423f739
Naresh Ban <nareshban40@gmail.com>
2025-01-07 18:11:49 +0545

    feat(workflow): update Lighthouse CI assertions for performance and accessibility categories

--- apps/web/lighthouserc.cjs
+++ apps/web/lighthouserc.cjs
@@ -1,3 +1,5 @@
+const { assert } = require("console");
+
 module.exports = {
   ci: {
     collect: {
@@ -6,7 +8,14 @@ module.exports = {
       numberOfRuns: 3,
     },
     assert: {
-      preset: "lighthouse:no-pwa",
+      // preset: "lighthouse:no-pwa",
+      assertions: {
+        "categories:performance": ["warn", { minScore: 0.9 }],
+        "categories:accessibility": ["warn", { minScore: 0.9 }],
+        "categories:best-practices": ["warn", { minScore: 0.9 }],
+        "categories:seo": ["warn", { minScore: 0.9 }],
+        "categories:pwa": ["off"],
+      },
     },
     upload: {
       target: "temporary-public-storage",
